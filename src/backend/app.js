37e15bc56819772b347e94f0da4fadde9be25399
sho0n <shon.sanches@gmail.com>
2019-09-01 10:53:13 +0100

    backend

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,7 +1,11 @@
+    
 const express = require('express')
+const shows = require('./shows')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('./rest/shows', (req, res) => {
+    res.send(shows());
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ src/backend/shows.js
@@ -0,0 +1,7 @@
+    
+module.exports = function shows() {
+    return [
+        { id: 'crown', name: 'crown', description: `This drama follows the political rivalries and romance of Queen Elizabeth 
+        II's reign and the events that shaped the second half of the 20th century.` },
+    ]
+}
