2a1c9e7e9a86f260efbc9d64ed5ba60e70a3976e
shon <shon.sanches@gmail.com>
2020-01-07 21:47:57 +0000

    push changes to heroku

--- src/backend/app.js
+++ src/backend/app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const getShows = require('./shows');
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 const path = require('path');
 
 
